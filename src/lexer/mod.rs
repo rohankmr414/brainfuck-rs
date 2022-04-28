8cabe3e9e135cb1a97fb34bf48b922b69bd65591
Rohan Kumar <rohankmr414@gmail.com>
2022-04-28 21:32:31 +0530

    feat: implemented lexer

---
+++ src/lexer/mod.rs
@@ -0,0 +1,167 @@
+use std::fmt;
+use std::fs::File;
+use std::io::Read;
+
+enum Error {
+    InvalidCharacter(char, i32, i32),
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Error::InvalidCharacter(c, pos, line) => write!(
+                f,
+                "Invalid character '{}' at position {}, line {}",
+                c, pos, line
+            ),
+        }
+    }
+}
+
+// TokenType for brainfuck lexer
+#[derive(Copy, Clone, PartialEq)]
+enum TokenType {
+    // Initialize a one-dimensional array of 30,000 elements with all the values set to 0.
+    PStart,
+    // End of file
+    EOF,
+    // Newline
+    NewLine,
+    // > Increment the data pointer (to point to the next cell to the right)
+    IncPtr,
+    // < Decrement the data pointer (to point to the next cell to the left)
+    DecPtr,
+    // + Increment (increase by one) the byte at the data pointer
+    IncByte,
+    // - Decrement (decrease by one) the byte at the data pointer
+    DecByte,
+    // . Output the byte at the data pointer
+    WriteByte,
+    // , Accept one byte of input, storing its value in the byte at the data pointer
+    ReadByte,
+    // [ Jump to the matching ] if the byte at the data pointer is zero
+    LoopStart,
+    // ] Jump to the matching [ if the byte at the data pointer is nonzero
+    LoopEnd,
+}
+
+struct Token {
+    token_type: TokenType,
+    line: i32,
+    pos: i32,
+}
+
+pub struct Tokenizer {
+    input: String,
+    ptr: usize,
+    pos: i32,
+    line: i32,
+    cur_tok: TokenType,
+    tokens: Vec<Token>,
+}
+
+trait Lexer {
+    fn new(input: &File) -> Self;
+    fn pos(&self) -> i32;
+    fn line(&self) -> i32;
+    fn next(&mut self) -> Result<TokenType, Error>;
+    fn send(&mut self);
+    fn lex(&mut self) -> Result<(), Error>;
+}
+
+impl Lexer for Tokenizer {
+    fn new(mut input: &File) -> Self {
+        let mut strinput = String::new();
+        input.read_to_string(&mut strinput).unwrap();
+        Tokenizer {
+            input: strinput,
+            ptr: 0,
+            pos: 1,
+            line: 1,
+            cur_tok: TokenType::PStart,
+            tokens: Vec::new(),
+        }
+    }
+
+    fn pos(&self) -> i32 {
+        self.pos
+    }
+
+    fn line(&self) -> i32 {
+        self.line
+    }
+
+    fn next(&mut self) -> Result<TokenType, Error> {
+        let c = self.input.chars().nth(self.ptr).unwrap();
+        let tok: TokenType;
+        match c {
+            '>' => {
+                tok = TokenType::IncPtr;
+                self.pos += 1;
+            }
+            '<' => {
+                tok = TokenType::DecPtr;
+                self.pos += 1;
+            }
+            '+' => {
+                tok = TokenType::IncByte;
+                self.pos += 1;
+            }
+            '-' => {
+                tok = TokenType::DecByte;
+                self.pos += 1;
+            }
+            '.' => {
+                tok = TokenType::WriteByte;
+                self.pos += 1;
+            }
+            ',' => {
+                tok = TokenType::ReadByte;
+                self.pos += 1;
+            }
+            '[' => {
+                tok = TokenType::LoopStart;
+                self.pos += 1;
+            }
+            ']' => {
+                tok = TokenType::LoopEnd;
+                self.pos += 1;
+            }
+            '\n' => {
+                tok = TokenType::NewLine;
+                self.line += 1;
+                self.pos = 1;
+            }
+            _ => {
+                return Err(Error::InvalidCharacter(c, self.pos, self.line));
+            }
+        }
+        self.ptr += 1;
+        Ok(tok)
+    }
+
+    fn send(&mut self) {
+        if self.cur_tok == TokenType::NewLine {
+            self.line += 1;
+            self.pos = 1;
+        }
+        let tok = Token {
+            token_type: self.cur_tok,
+            line: self.line,
+            pos: self.pos,
+        };
+        self.tokens.push(tok)
+    }
+
+    fn lex(&mut self) -> Result<(), Error> {
+        while self.ptr < self.input.len() {
+            let tok = self.next();
+            match tok {
+                Ok(tok) => self.cur_tok = tok,
+                Err(e) => return Err(e)
+            };
+            self.send();
+        }
+        Ok(())
+    }
+}
